e60196775aa0db801c77794569eea8117ba64710
Alex Draev <a.draev@codecoda.com>
2021-02-24 08:04:32 +0200

    User Model & Authentication Service created, bootstrap imported

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,9 +4,10 @@ CREATE TABLE users (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   first_name VARCHAR(250) NOT NULL,
   last_name VARCHAR(250) NOT NULL,
-  role VARCHAR(250) DEFAULT NULL
+  password VARCHAR(250) DEFAULT NULL
 );
 
-INSERT INTO users (first_name, last_name, role) VALUES
+
+INSERT INTO users (first_name, last_name, password) VALUES
 ('Publisher', 'Publisher', 'Publisher'),
 ('Viewer', 'Viewer', 'Viewer');
