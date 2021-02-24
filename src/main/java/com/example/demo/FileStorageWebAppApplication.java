cda10b074fca03c3248647fb3a5aaf67dea5ffa7
Alex Draev <a.draev@codecoda.com>
2021-02-25 01:05:14 +0200

    Basic File Upload functionality imported

--- src/main/java/com/example/demo/FileStorageWebAppApplication.java
+++ src/main/java/com/example/demo/FileStorageWebAppApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.properties.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@EnableConfigurationProperties(StorageProperties.class)
 public class FileStorageWebAppApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.response.FileResponse;
+import com.example.demo.service.StorageService;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.stream.Collectors;
+
+@Controller
+public class FileController {
+
+    private StorageService storageService;
+
+    public FileController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping("/publisher/listAll")
+    public String listAllFiles(Model model) {
+
+        model.addAttribute("files", storageService.loadAll().map(
+                path -> ServletUriComponentsBuilder.fromCurrentContextPath()
+                        .path("/download/")
+                        .path(path.getFileName().toString())
+                        .toUriString())
+                .collect(Collectors.toList()));
+
+        return "publisherPage";
+    }
+
+    @GetMapping("/download/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
+
+        Resource resource = storageService.loadAsResource(filename);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+    @PostMapping("/upload-file")
+    @ResponseBody
+    public FileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+        String name = storageService.store(file);
+
+        String uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/download/")
+                .path(name)
+                .toUriString();
+
+        return new FileResponse(name, uri, file.getContentType(), file.getSize());
+    }
+}
